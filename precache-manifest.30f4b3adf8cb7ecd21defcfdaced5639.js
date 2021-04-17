d34bc9ff5d115a64d02a593cccf14bc00b743059
Mick Koryliuk <craftbazinga@gmail.com>
2021-04-17 13:10:19 -0400

    Updates

---
+++ precache-manifest.30f4b3adf8cb7ecd21defcfdaced5639.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ebce849cfb079bae9e8c933028bfd4",
+    "url": "/github_finder_react_2020_2021/index.html"
+  },
+  {
+    "revision": "6c5bf7d6d65325144bbd",
+    "url": "/github_finder_react_2020_2021/static/css/main.bc1a1419.chunk.css"
+  },
+  {
+    "revision": "23f955efd6e4dae4d488",
+    "url": "/github_finder_react_2020_2021/static/js/2.30d23f95.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github_finder_react_2020_2021/static/js/2.30d23f95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c5bf7d6d65325144bbd",
+    "url": "/github_finder_react_2020_2021/static/js/main.35435e14.chunk.js"
+  },
+  {
+    "revision": "a8adb5a7752f57323eb1",
+    "url": "/github_finder_react_2020_2021/static/js/runtime-main.d04fe9f4.js"
+  }
+]);
